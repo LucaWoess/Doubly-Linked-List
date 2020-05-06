1ea5b3be5ee8a2dbfb19fc96e4997003905db757
LucaWoess <luwoess@tsn.at>
2020-05-06 21:01:49 +0200

    Added a few Methods

--- src/DoublyLinkedListMain.java
+++ src/DoublyLinkedListMain.java
@@ -2,9 +2,11 @@ public class DoublyLinkedListMain {
 
 	public static void main(String[] args) {
 		List woess = new List(5);
-		woess.addElement(7);
-		woess.addElement(7);
-		woess.addElement(77);
+		woess.addElementLastPlace(7);
+		woess.addElementLastPlace(7);
+		woess.addElementLastPlace(77);
+		woess.addElementFirstPlace(55);
+		woess.deleteSpecificElement(10);
 		woess.writeList();
 	}
 
--- src/List.java
+++ src/List.java
@@ -13,11 +13,68 @@ public class List {
 		return lastElement;
 	}
 	
-	public void addElement(int value) {
+	public void addElementLastPlace(int value) {
 		Element lastElement = getLastElement();
 		Element newElement = new Element(value);
 		lastElement.nextAddress = newElement;
 		newElement.prevAddress = lastElement;
+	}
+	
+	public void addElementFirstPlace(int value) {
+		Element newElement = new Element(value);
+		Element elem = getLastElement();
+		addElementLastPlace(0);
+		while(elem != null) {
+			elem.nextAddress.value = elem.value;
+			elem = elem.prevAddress;
+		}
+		headElement.value = newElement.value;
+	}
+	
+	public void addSpecificElement(int index,int value) {
+		Element newElement = new Element(value);
+		if(index == 0) {
+			addElementFirstPlace(value);
+			return;
+		}
+		if(index>=length()) {
+			System.out.println("The List isn´t that long!");
+			return;	
+		}
+		if(index<0) {
+			System.out.println("The number must be positive!");
+			return;
+		}
+		addElementLastPlace(0);
+		while(index < length()-1) {
+			
+			
+		}
+	}
+	
+	public void deleteSpecificElement(int index) {
+		Element deleteElement = headElement;
+		Element lastElement = null;
+		if(index==0) {
+			headElement=headElement.nextAddress;
+			return;
+		}
+		if(index<0) {
+			System.out.println("The number must be positive!");
+			return;
+		}
+		if(index>=length()) {
+			System.out.println("The List isn´t that long!");
+			return;
+		}
+		while(index > 0)
+		{
+			lastElement = deleteElement;
+			deleteElement = deleteElement.nextAddress;
+			index--;
+		}
+		lastElement.nextAddress = deleteElement.nextAddress;
+
 	}
 	
 	public void writeList() {
@@ -28,13 +85,13 @@ public class List {
 		}
 	}
 
-	/*public int lenght() {
+	public int length() {
 		int lenght = 1;
-		Element nextElement = headElem.address;
+		Element nextElement = headElement.nextAddress;
 		while(nextElement != null) {
 			lenght++;
-			nextElement=nextElement.address;
+			nextElement=nextElement.nextAddress;
 		}
 		return lenght;
-	}*/
+	}
 }
